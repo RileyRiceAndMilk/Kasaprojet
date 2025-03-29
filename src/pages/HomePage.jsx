55f2e675a17ea40c0d46422a422f6fd4db31f1c8
RileyRiceAndMilk <167301632+RileyRiceAndMilk@users.noreply.github.com>
2025-03-29 13:10:01 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "../css/style.css";
-import logo from "../assets/logo.png";
+import logo from "src/assets/logo.png";
 
 const HomePage = () => {
 
